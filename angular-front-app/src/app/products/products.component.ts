1f2a54342f7f2eca97e8e00211fd4a6ce06ce4b3
saidnidmessaoud <nidmessaoudsaid1@gmail.com>
2024-01-29 01:15:31 +0000

    V1

---
+++ angular-front-app/src/app/products/products.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+// import * as console from "console";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit{
+  products : any;
+  constructor(private http : HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get("http://localhost:8098/products")
+      .subscribe({
+         next : data => {
+           this.products = data;
+         },
+        error : err => {
+         console.log(err);
+        }
+      });
+  }
+}
---
+++ customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/model/Product.java
@@ -0,0 +1,12 @@
+package ma.emsi.customerfrontthymeleafapp.model;
+
+import lombok.*;
+
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter @Builder
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+}
